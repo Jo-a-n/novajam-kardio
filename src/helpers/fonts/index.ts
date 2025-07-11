f965f3172ee9bd052e0f177733dc97d10e6a4074
Jo-a-n <56139906+Jo-a-n@users.noreply.github.com>
2025-07-11 14:36:51 +0300

    Removes unused fonts

--- src/helpers/fonts/index.ts
+++ src/helpers/fonts/index.ts
@@ -1,11 +1,5 @@
 import classNames from 'classnames';
-import {
-  Roboto,
-  Poppins,
-  Montserrat,
-  Inter,
-  Playfair_Display,
-} from 'next/font/google';
+import { Roboto, Inter } from 'next/font/google';
 
 const Roboto_Font = Roboto({
   subsets: ['latin', 'latin-ext', 'greek', 'greek-ext'],
@@ -17,34 +11,11 @@ const Inter_Font = Inter({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
 });
 
-const Poppins_Font = Poppins({
-  subsets: ['latin', 'latin-ext'],
-  weight: ['100', '200', '300', '400', '500', '600', '700'],
-  variable: '--font-heading',
-});
-
-const Montserrat_Font = Montserrat({
-  subsets: ['latin', 'latin-ext', 'vietnamese'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-  variable: '--font-heading',
-});
-
-const Playfair_Display_Font = Playfair_Display({
-  subsets: ['latin', 'latin-ext', 'vietnamese'],
-  weight: ['400', '500', '600', '700', '800'],
-  variable: '--font-heading',
-});
-
 export const generateFontClassnames = (
   fontMain: string | null,
   fontHeading: string | null,
 ) =>
   classNames({
     [Roboto_Font.className]: fontMain === 'Roboto',
-    [Inter_Font.className]: fontMain === 'Inter',
-    [Montserrat_Font.className]: fontMain === 'Montserrat',
-    [Montserrat_Font.variable]: fontHeading === 'Montserrat',
-    [Poppins_Font.className]: fontMain === 'Poppins',
-    [Poppins_Font.variable]: fontHeading === 'Poppins',
-    [Playfair_Display_Font.variable]: fontHeading === 'Playfair Display',
+    [Inter_Font.className]: fontHeading === 'Inter',
   });
