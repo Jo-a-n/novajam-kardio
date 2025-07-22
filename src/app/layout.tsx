972de2904edfd27ec1edad2448ed3a8d3ef7b207
Jo-a-n <56139906+Jo-a-n@users.noreply.github.com>
2025-07-22 15:50:36 +0300

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import '@/app/styles/globals.css';
 import { PostHogProvider } from '@/components/PostHogProvider';
+import Script from 'next/script';
 
 export default async function RootLayout({
   children,
@@ -10,6 +11,23 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-0GGQJPH7FV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0GGQJPH7FV');
+          `}
+        </Script>
+      </head>
       <body className="bg-[#EBEDF4] dark:bg-neutral-900 dark:text-neutral-100">
         <PostHogProvider>
           {children}
