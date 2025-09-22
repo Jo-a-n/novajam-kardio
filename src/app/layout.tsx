48aaf10649f316a00cce32a6d5a13a2ff9acac00
Pavlos Vinieratos <pvinis@gmail.com>
2025-09-22 09:40:46 +0300

    remove posthog analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import '@/app/styles/globals.css';
-import { PostHogProvider } from '@/components/PostHogProvider';
 import Script from 'next/script';
 
 export default async function RootLayout({
@@ -29,11 +28,9 @@ export default async function RootLayout({
         </Script>
       </head>
       <body className="bg-[#EBEDF4] dark:bg-neutral-900 dark:text-neutral-100">
-        <PostHogProvider>
-          {children}
-          <Analytics />
-          <SpeedInsights />
-        </PostHogProvider>
+        {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
